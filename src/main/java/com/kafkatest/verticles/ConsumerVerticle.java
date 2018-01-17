ff4b210996da8b2b4d19d3b232effb16966a46d6
Gavrail <gavrail.kubatev@draemix.eu>
2018-01-17 18:10:08 +0200

    multiple test updates

--- src/main/java/com/kafkatest/verticles/ConsumerVerticle.java
+++ src/main/java/com/kafkatest/verticles/ConsumerVerticle.java
@@ -1,11 +1,14 @@
 package com.kafkatest.verticles;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.kafka.client.common.TopicPartition;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
 import kafka.Kafka;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Sleeper on 16/01/2018.
@@ -14,24 +17,81 @@ public class ConsumerVerticle extends AbstractVerticle{
 
     @Override
     public void start(){
-        KafkaConsumer<String,String> consumer = createKafkaConsumer("my_group");
+        //Consumers with Subscribe
+        KafkaConsumer<String,String> consumerSubscriber1 = createKafkaConsumer("my_group");
 
-        consumer.handler(record -> {
-            System.out.println("Processing key=" + record.key() + ",value=" + record.value() +
+        consumerSubscriber1.handler(record -> {
+            System.out.println("ConsumerSubscriber1: Processing key=" + record.key() + ",value=" + record.value() +
                     ",partition=" + record.partition() + ",offset=" + record.offset());
         });
+        Set<String> topics = new HashSet<>();
+        topics.add("test1");
+        topics.add("test2");
+        topics.add("repTopic");
+        consumerSubscriber1.subscribe(topics);
+     /*
+     //Consumers with Assign
+        KafkaConsumer<String,String> consumerAssigner1 = createKafkaConsumer("assignerGroup");
+        consumerAssigner1.handler(record->{
+            System.out.println("ConsumerAssigner1: Processing key=" + record.key() + ",value=" + record.value() +
+                    ",partition=" + record.partition() + ",offset=" + record.offset());
+        });
+        Set<TopicPartition> topicPartitions = new HashSet<>();
+        topicPartitions.add(new TopicPartition()
+                .setTopic("repTopic")
+                .setPartition(1));
+        // registering handlers for assigned and revoked partitions
+        consumerAssigner1.partitionsAssignedHandler(topicPartitionsInput -> {
 
-        consumer.subscribe("test");
+            System.out.println("Partitions assigned");
+            for (TopicPartition topicPartition : topicPartitionsInput) {
+                System.out.println(topicPartition.getTopic() + " " + topicPartition.getPartition());
+            }
+        });
+
+        consumerAssigner1.partitionsRevokedHandler(topicPartitionsInput -> {
+
+            System.out.println("Partitions revoked");
+            for (TopicPartition topicPartition : topicPartitionsInput) {
+                System.out.println(topicPartition.getTopic() + " " + topicPartition.getPartition());
+            }
+        });
+        consumerAssigner1.assign(topicPartitions,done -> {
+
+            if (done.succeeded()) {
+                System.out.println("Partition assigned");
+
+                // requesting the assigned partitions
+                consumerAssigner1.assignment(done1 -> {
+
+                    if (done1.succeeded()) {
+
+                        for (TopicPartition topicPartition : done1.result()) {
+                            System.out.println(topicPartition.getTopic() + " " + topicPartition.getPartition());
+                        }
+                    }
+                });
+            }else{
+                System.out.println("Failure during partitions assignement");
+            }
+        });
+
+       /* KafkaConsumer<String,String> consumerAssigner2 = createKafkaConsumer("assignerGroup");
+        consumerAssigner2.handler(record->{
+            System.out.println("ConsumerAssigner2: Processing key=" + record.key() + ",value=" + record.value() +
+                    ",partition=" + record.partition() + ",offset=" + record.offset());
+        });
+        consumerAssigner2.assign(topicPartitions);*/
     }
 
     private KafkaConsumer<String,String> createKafkaConsumer(String group){
         Map<String,String> config = new HashMap<>();
-        config = new HashMap<>();
-        config.put("bootstrap.servers", "localhost:9092");
+        config.put("bootstrap.servers", "localhost:9093,localhost:9094");
         config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        config.put("group.id", group);
-        config.put("auto.offset.reset", "earliest");
+        if(group != null)
+            config.put("group.id", group);
+       // config.put("auto.offset.reset", "earliest");
         config.put("enable.auto.commit", "false");
 
         return KafkaConsumer.create(vertx,config);
--- src/main/java/com/kafkatest/verticles/ProducerVerticle.java
+++ src/main/java/com/kafkatest/verticles/ProducerVerticle.java
@@ -2,6 +2,7 @@ package com.kafkatest.verticles;
 
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
 
@@ -18,24 +19,38 @@ public class ProducerVerticle extends AbstractVerticle {
 
         vertx.createHttpServer().requestHandler(
                 r->{
-                    if( r.getParam("text")!= null ) {
-                        System.out.println("Value to produce: " +r.getParam("text"));
-                        KafkaProducer<String,String> producer = createProducer();
-                        KafkaProducerRecord<String, String> record =
-                                KafkaProducerRecord.create("test", r.getParam("text"));
-                        producer.write(record);
-                    }
-                 r.response().end("Received " +  r.getParam("text"));
+                    handleRequest(r);
+                    r.response().end("Received " +  r.getParam("text"));
                 }).listen(8080);
+    }
+
+
+    private void  handleRequest(HttpServerRequest request){
+        if( request.getParam("text")!= null ) {
+            System.out.println("Value to produce: " +request.getParam("text"));
+            KafkaProducer<String,String> producer = createProducer();
+            KafkaProducerRecord<String, String> record =
+                    KafkaProducerRecord.create("test1", request.getParam("text") + " on test1");
+            KafkaProducerRecord<String, String> record2 =
+                    KafkaProducerRecord.create("test2",request.getParam("text") + " on test2");
+            KafkaProducerRecord<String, String> recordPartitioned =
+                    //KafkaProducerRecord.create("repTopic", "mykey1",request.getParam("text") + " on repTopic",1);
+                    KafkaProducerRecord.create("repTopic", request.getParam("text") + " on repTopic");
+            //producer.write(record);
+            //producer.write(record2);
+            producer.write(recordPartitioned);
+        }
     }
 
     private  KafkaProducer<String,String> createProducer(){
         Map<String, String> config = new HashMap<>();
-        config.put("bootstrap.servers", "localhost:9092");
+        config.put("bootstrap.servers", "localhost:9093,localhost:9094");
         config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        config.put("acks", "1");
+        //config.put("acks", "1");
         KafkaProducer<String, String> producer = KafkaProducer.create(vertx, config);
         return producer;
+
+
     }
 }
